06839fa38facde4e1b16efdceaaebda9c0081e9f
Reed MacDonald <reedpmacdonald@gmail.com>
2019-10-08 13:10:18 -0700

    most of the stuff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import './App.css'
+import SmallerDiv from './Components/SmallerDiv'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component{
+    state={
+      clicked:false,
+      endeth:'no',
+      turn:false,
+      transDur:undefined
+    }
+    changeThings=()=>{
+      if(this.state.clicked){
+        setTimeout(this.close,200)
+        this.setState({
+          endeth:'yes',
+          turn:false,
+          transDur:'.5s'
+        })
+        console.log('Closing in five seconds')
+      }
+      else{
+        this.setState({
+          clicked:true,
+          endeth:'no',
+          turn:true,
+          transDur:undefined
+        })
+        console.log('Clicked')
+      }
+    }
+    close=()=>{
+      this.setState({
+        clicked:false
+      })
+    }
+  render(){
+    return(
+  <div>
+    <div onClick={this.changeThings}id='bigButton'>
+      {this.state.clicked?<SmallerDiv endeth={this.state.endeth}/>:undefined}
+
+      <div style={{transitionDuration:this.state.transDur,transform:this.state.turn?'rotate(-45deg)':undefined}} id='letters'>+</div>
+      </div>
+  </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/Components/SmallerDiv.js
@@ -0,0 +1,73 @@
+import React, {Component} from 'react'
+import '../App.css'
+
+
+
+
+class SmallerDiv extends Component{
+    state={
+      clicked:false,
+      height:'10px',
+      width:'10px',
+      folderTop:'50%',
+      pdfTop:'50%',
+      transform:undefined,
+      marginLeft:'-10px'
+    }
+    componentDidMount(){
+        setTimeout(this.makeBig,0)
+        setTimeout(this.giveHeight,200)
+     
+        setTimeout(this.moveUp,200)
+        setTimeout(this.moveAgain,400)
+    }
+    makeBig=()=>{
+        this.setState({
+            width:'80px',
+            folderTop:'28%',
+            pdfTop:'15%',
+            marginLeft:undefined
+        })
+    }
+
+    moveUp=()=>{
+        this.setState({
+            folderTop:'30%',
+            pdfTop:'17%'
+        })
+    }
+    giveHeight=()=>{
+        this.setState({
+            height:'80px',
+        })
+    }
+    moveAgain=()=>{
+        this.setState({
+            folderTop:'28%',
+            pdfTop:'15%'
+        })
+    }
+    componentDidUpdate(prevProps){
+        if(this.props.endeth!==prevProps.endeth){
+
+            this.setState({
+                height:'10px',
+                width:'10px',
+                folderTop:'50%',
+                pdfTop:'50%',
+                marginLeft:'-10px'
+            })
+        }
+    }
+  render(){
+    return(
+  <div>
+      <div style={{marginLeft:this.state.marginLeft, height:this.state.height,width:this.state.width, top:this.state.folderTop, transitionDuration:'.2s',transform:this.state.transform}} id='fileDiv' class='smallerDiv'><img id='folder' src='http://icons.iconarchive.com/icons/dakirby309/simply-styled/256/Blank-Folder-icon.png'></img></div>
+      <div style={{marginLeft:this.state.marginLeft, height:this.state.height,width:this.state.width, top:this.state.pdfTop, transitionDuration:'.2s',transform:this.state.transform}} id='pdfDiv' class='smallerDiv'><img id='pdf' src='https://cdn3.iconfinder.com/data/icons/line-icons-set/128/1-02-512.png'></img></div>
+      
+  </div>
+    )
+  }
+}
+
+export default SmallerDiv
